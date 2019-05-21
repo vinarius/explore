e469bccb6804b14a05e5afa1bb193f3b764329c6
Mark <vindevccm@gmail.com>
2019-05-21 14:50:38 -0400

    completed project 1 - starbreak coffee - bar chart revenue by month

--- udemy/d3-adam_janes-mastering_data_visualization_in_d3/Projects/Starbreak_Coffee/js/main.js
+++ udemy/d3-adam_janes-mastering_data_visualization_in_d3/Projects/Starbreak_Coffee/js/main.js
@@ -6,10 +6,8 @@
 
 window.addEventListener('DOMContentLoaded', (event) => {
 
-
     d3.json('./data/revenues.json').then((_data) => {
         
-
         let data = _data.map((element) => {
             element.revenue = +element.revenue;
             element.profit = +element.profit;
@@ -17,14 +15,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
         });
 
         const margin = {
-            top: 10,
+            top: 30,
             right: 20,
-            bottom: 30,
-            left: 40
+            bottom: 70,
+            left: 75
         };
 
         const width = 600 - margin.right - margin.left;
-        const height = 400 - margin.top - margin.bottom;
+        const height = 500 - margin.top - margin.bottom;
 
         const chart = d3.select('#chart-area')
             .append('svg')
@@ -32,6 +30,24 @@ window.addEventListener('DOMContentLoaded', (event) => {
             .attr('height', height + margin.top + margin.bottom)
             .append('g')
                 .attr('transform', `translate(${margin.left},${margin.top})`);
+
+        // X label.
+        chart.append('text')
+            .attr('class', 'x-axis-label')
+            .attr('x', width / 2)
+            .attr('y', height + (margin.bottom / 1.5))
+            .attr('font-szie', '20px')
+            .attr('text-anchor', 'end')
+            .text('Month');
+
+        // Y label.
+        chart.append('text')
+            .attr('class', 'y-axis-label')
+            .attr('x', -height / 2)
+            .attr('y', -margin.left / 1.5)
+            .attr('font-size', '20px')
+            .attr('transform', 'rotate(-90)')
+            .text('Revenue');
 
         const y = d3.scaleLinear()
             .domain([0, d3.max(data, (d) => {
@@ -59,27 +75,24 @@ window.addEventListener('DOMContentLoaded', (event) => {
             .attr('transform', `translate(0, ${height})`)
             .call(bottomAxisCall);
 
+
+        const rects = chart.selectAll('rect')
+            .data(data);
+
+        rects.enter()
+            .append('rect')
+            .attr('x', (d) => {
+                return x(d.month);
+            })
+            .attr('y', (d) => {
+                return y(d.revenue);
+            })
+            .attr('width', x.bandwidth)
+            .attr('height', (d) => {
+                return height - y(d.revenue)
+            })
+            .attr('fill', '#444');
+
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }); // End of document ready.
