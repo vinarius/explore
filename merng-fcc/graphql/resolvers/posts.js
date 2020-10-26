5b7c73e3316f6a40f00256e8bddd14b59b728465
Mark <vindevccm@gmail.com>
2020-10-26 16:55:53 -0400

    finished basic graphql crud backend

--- merng-fcc/graphql/resolvers/posts.js
+++ merng-fcc/graphql/resolvers/posts.js
@@ -1,13 +1,78 @@
 const Post = require('../../models/Post');
+const checkAuth = require('../../utils/checkAuth');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 module.exports = {
     Query: {
         async getPosts(){
             try {
-                return await Post.find({}); // fetch all Post documents
+                return await Post.find().sort({ createdAt: -1 });
             } catch (error) {
                 throw new Error(error);
             }
+        },
+        async getPost(_, { postId }) {
+            try {
+                const post = await Post.findById(postId);
+                if (post) return post;
+                else throw new Error('Post not found');
+            } catch (error) {
+                throw new Error(error);
+            }
+        }
+    },
+    Mutation: {
+        async createPost(_, { body }, context) {
+            try {
+                const user = checkAuth(context);
+                console.log(user);
+
+                const newPost = new Post({
+                    body,
+                    user: user.indexOf,
+                    username: user.username,
+                    createdAt: new Date().toISOString()
+                });
+
+                return await newPost.save();
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+        async deletePost(_, { postId }, context) {
+            const user = checkAuth(context);
+
+            // Verify user created the post, no one else can delete it
+            try {
+                const post = await Post.findById(postId);
+                if(user.username === post.username) {
+                    await post.delete();
+                    return 'Post deleted successfully';
+                } else {
+                    throw new AuthenticationError('Action not allowed');
+                }
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+        async likePost(parent, args, context) {
+            const { postId } = args;
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+            if(post) {
+                if(post.likes.find(like => like.username === username)) {
+                    // Post already liked
+                    post.likes = post.likes.filter(like => like.username !== username);
+                } else {
+                    // Post not liked
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    });
+                }
+                await post.save();
+            } else throw new UserInputError('post not found');
         }
     }
 };
--- merng-fcc/graphql/typeDefs.js
+++ merng-fcc/graphql/typeDefs.js
@@ -6,6 +6,21 @@ type Post {
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+    likes: [Like]!
+    commentCount: Int!
+    likeCount: Int!
+}
+type Comment {
+    id: ID!
+    createdAt: String!
+    username: String!
+    body: String!
+}
+type Like {
+    id: ID!
+    createdAt: String!
+    username: String!
 }
 type User {
     id: ID!
@@ -22,9 +37,15 @@ input RegisterInput {
 }
 type Query {
     getPosts: [Post]
+    getPost(postId: ID!): Post
 }
 type Mutation {
     register(registerInput: RegisterInput): User!
     login(username: String!, password: String!): User!
+    createPost(body: String!): Post!
+    deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
 }
 `;
